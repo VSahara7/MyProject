8aa27e93bcecd415177e533a1bac9750aeed89ad
VSahara7 <88375135+VSahara7@users.noreply.github.com>
2023-03-07 13:46:08 +0545

    this is my first project01~

---
+++ Assets/Js/min.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function myFunction1() {
+    var x = document.getElementById("myTopnav1");
+    if (x.className === "topnav1") {
+      x.className += " responsive1";
+    } else {
+      x.className = "topnav1";
+    }
+  }
